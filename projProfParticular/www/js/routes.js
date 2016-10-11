d06ce49d160b9176e9a4124028552972f72d8381
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-11 17:05:27 -0300

    Commit inicial tudo funcionando

---
+++ projProfParticular/www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.professores', {
+    url: '/professores',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/professores.html',
+        controller: 'professoresCtrl'
+      }
+    }
+  })
+
+  .state('menu.historico', {
+    url: '/historico',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/historico.html',
+        controller: 'historicoCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/home')
+
+  
+
+});
