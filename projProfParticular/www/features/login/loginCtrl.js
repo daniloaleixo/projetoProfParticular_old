9089e21ded9200dd14f8d933b84b5cb2fc05759b
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-11 20:49:58 -0300

    Consegui fazer o login com o google

--- projProfParticular/www/features/login/loginCtrl.js
+++ projProfParticular/www/features/login/loginCtrl.js
@@ -3,7 +3,7 @@ angular.module('app.controllers', [])
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $ionicLoading) {
+function ($scope, $stateParams, $ionicLoading, $firebaseAuth) {
 	var loginCtrl = this;
 
 	loginCtrl.user = {
@@ -32,12 +32,12 @@ function ($scope, $stateParams, $ionicLoading) {
 		loginCtrl.error = '';
 
 		//A senha do firebase deve ser maior que 6 caracteres
-		if(loginCtrl.user.password.length < 6) {
+		if(loginCtrl.user.password.length < 6 && loginCtrl.user.password.length != 0) {
 			loginCtrl.error = "Sua senha deve ser maior que 6 caracteres";
 			return ;
 		}	
 
-
+		//Se for pra registrar manda pra la
 		if(loginCtrl.signingUp) loginCtrl.register();
 		else {
 			console.log("Vou tentar fazer login");
@@ -75,6 +75,28 @@ function ($scope, $stateParams, $ionicLoading) {
 		} else {
 			loginCtrl.error = "As senhas não coincidem";
 		}
+	};
+
+	loginCtrl.googleLogin = function(){
+		console.log("Vou tentear fazer o login com o google");
+
+		var provider = new firebase.auth.GoogleAuthProvider();
+
+  		var tryGoogleSignIn = firebase.auth().signInWithPopup(provider);
+  		showLoading();
+  		tryGoogleSignIn.then(function(result) {
+		  // This gives you a Google Access Token. You can use it to access the Google API.
+		  var token = result.credential.accessToken;
+		  // The signed-in user info.
+		  var user = result.user;
+		  console.log("User " + user);
+		  hideLoading();
+		}).catch(function(error){
+					  // Handle Errors here.
+		  var errorCode = error.code;
+		  loginCtrl.error = error.message;
+		  hideLoading();
+		});
 	};
 
 
