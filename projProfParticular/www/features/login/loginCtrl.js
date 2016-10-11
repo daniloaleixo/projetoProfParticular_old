d0655051fbb095066f465bb2db37cc1a95db32f2
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-11 20:01:11 -0300

    Adicionei o gif para carregar as infos do servidor

--- projProfParticular/www/features/login/loginCtrl.js
+++ projProfParticular/www/features/login/loginCtrl.js
@@ -1,5 +1,5 @@
 angular.module('app.controllers', [])
-.controller('LoginCtrl', ['$scope', '$stateParams',
+.controller('LoginCtrl', ['$scope', '$stateParams', '$ionicLoading',
 // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -13,6 +13,17 @@ function ($scope, $stateParams, $ionicLoading) {
 	};
 	loginCtrl.signingUp = false;
 	loginCtrl.error = '';
+
+	var showLoading = function(){
+		$ionicLoading.show({
+			template: '<ion-spinner icon="spiral"></ion-spinner>',
+			noBackdrop: true
+		});
+	}
+
+	var hideLoading = function(){
+		$ionicLoading.hide();
+	}
 
 
 	//console.log("Estou no controller");
@@ -31,11 +42,16 @@ function ($scope, $stateParams, $ionicLoading) {
 		else {
 			console.log("Vou tentar fazer login");
 
-			firebase.auth().signInWithEmailAndPassword(loginCtrl.user.email, loginCtrl.user.password)
-			.then(function(auth){
+			var trySignIn = firebase.auth().signInWithEmailAndPassword(loginCtrl.user.email, loginCtrl.user.password);
+
+			showLoading();
+
+			trySignIn.then(function(auth){
 				console.log("Estou logado como " + auth.email);
+				hideLoading();
 			}, function(error){
 				loginCtrl.error = "Não foi possivel fazer o login, verifique o email e a senha";
+				hideLoading();
 			});
 		}
 	};
@@ -44,11 +60,17 @@ function ($scope, $stateParams, $ionicLoading) {
 		//Compara as senhas
 		if(loginCtrl.user.password == loginCtrl.user.password2){
 			console.log("Vou tentar me registrar");
-			firebase.auth().createUserWithEmailAndPassword(loginCtrl.user.email, loginCtrl.user.password)
-			.then(function(user){
+
+			var tryRegister = firebase.auth().createUserWithEmailAndPassword(loginCtrl.user.email, loginCtrl.user.password);
+
+			showLoading();
+
+			tryRegister.then(function(user){
 				console.log("Consegui me registrar");
+				hideLoading();
 			},function(error){
 				loginCtrl.error = "Não foi possivel fazer o cadastro, verifique o email e a senha";
+				hideLoading();
 			})
 		} else {
 			loginCtrl.error = "As senhas não coincidem";
