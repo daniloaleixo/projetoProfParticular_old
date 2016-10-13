411ec501d22b29eae5e9149d40e7995e6d363e10
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-13 17:19:52 -0300

    Tentando incluir o modulo de profile

---
+++ projProfParticular/www/features/profile/profileCtrl.js
@@ -0,0 +1,11 @@
+angular.module('app.controllers')
+.controller('ProfileCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	var profileCtrl = this;
+	
+	//console.log("home Ctrl:");
+	console.log("ProfileCtrl| : estou aqui ");
+
+}]);
