67b5edd6a8e14f05cf6904e202fff94e382fb33f
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-17 16:34:01 -0200

    Nome do usuario esta atualizando falta perpetuar isso para o sistema

--- projProfParticular/www/features/profile/profileCtrl.js
+++ projProfParticular/www/features/profile/profileCtrl.js
@@ -4,4 +4,43 @@ appProf
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 	var profileCtrl = this;
+
+	profileCtrl.user = {
+		displayName: '',
+		photoURL: '',
+		email: ''
+	};
+
+	profileCtrl.newUserInfos = {
+		displayName: '',
+		photoURL: '',
+		email: ''
+	};
+
+	if(firebaseUser != null){
+		console.log("ProfileCtrl| Menu: displayName: " + firebaseUser.displayName);
+		console.log("ProfileCtrl| Menu: photoURL: " + firebaseUser.photoURL);
+		profileCtrl.user.displayName = firebaseUser.displayName || firebaseUser.email;
+		profileCtrl.user.photoURL = firebaseUser.photoURL || 'img/login-bkg.jpg';
+		profileCtrl.user.email = firebaseUser.email || '';
+
+		console.log("ProfileCtrl| ProfileCtrl: displayName: " + profileCtrl.user.displayName);
+		console.log("ProfileCtrl| ProfileCtrl: photoURL: " + profileCtrl.user.photoURL);
+
+	}
+
+	profileCtrl.changeDisplayName = function(){
+		console.log("Estou aqui");
+		if(profileCtrl.newUserInfos.displayName != '' && firebaseUser != null){
+			firebaseUser.updateProfile({
+				displayName: profileCtrl.newUserInfos.displayName
+			}).then(function(){
+				console.log("ProfileCtrl |Display Name foi atualizado com sucesso!");
+			}, function(error){
+				console.log("ProfileCtrl |Erro ao atualizar Display Name");
+			});
+		}
+	};
+
+
 }])
