96677c5ad96ae6e09c1b775f77661428fa642aaf
Danilo Aleixo <danilo_aleixo@hotmail.com>
2016-10-20 15:23:06 -0200

    Resolvi o problema de sincronismo nos professores

--- projProfParticular/www/features/professores/professoresCtrl.js
+++ projProfParticular/www/features/professores/professoresCtrl.js
@@ -1,11 +1,11 @@
 
 appProf
-.controller('ProfessoresCtrl', ['$scope', '$stateParams', 'FIREBASE_CONFIG','ratingConfig',
+.controller('ProfessoresCtrl', ['$scope', '$stateParams', 'FIREBASE_CONFIG','ratingConfig', '$ionicLoading',
   // The following is the constructor function for this page's controller. 
   //See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, FIREBASE_CONFIG, ratingConfig) {
+function ($scope, $stateParams, FIREBASE_CONFIG, ratingConfig, $ionicLoading) {
 	professoresCtrl = this;
 
 	console.log("ProfessoresCtrl | estou aqui");
@@ -18,6 +18,17 @@ function ($scope, $stateParams, FIREBASE_CONFIG, ratingConfig) {
 	professoresCtrl.rating = {
 		'max': 5,
 		'readOnly': true
+	}
+
+	var showLoading = function(){
+		$ionicLoading.show({
+			template: '<ion-spinner icon="spiral"></ion-spinner>',
+			noBackdrop: true
+		});
+	}
+
+	var hideLoading = function(){
+		$ionicLoading.hide();
 	}
 
 	professoresCtrl.professores = [
@@ -36,6 +47,15 @@ function ($scope, $stateParams, FIREBASE_CONFIG, ratingConfig) {
 				"didatica":5,
 				"conhecimento":5,
 				"simpatia":5
+			},
+			"localizacoes":{
+				"principal": {
+					"logradouro": "Rua xyz, 180",
+					"cidade": "São Paulo ",
+					"estado":"SP",
+					"CEP": "05454555"
+				},
+				"secundararias" : []
 			},
 			"curriculo": {
 				"sobre":"Dou aulas particulares há 5 anos e ja fiz isso e isso e isso, sempre estou presente na vida da galera e quero um texto longo",
@@ -65,6 +85,15 @@ function ($scope, $stateParams, FIREBASE_CONFIG, ratingConfig) {
 				"conhecimento":5,
 				"simpatia":5
 			},
+			"localizacoes":{
+				"principal": {
+					"logradouro": "Rua xyz, 180",
+					"cidade": "Campinas",
+					"estado":"SP",
+					"CEP": "05454555"
+				},
+				"secundararias" : []
+			},
 			"curriculo": {
 				"sobre":"Sou muito legal",
 				"formacao":{
@@ -80,23 +109,29 @@ function ($scope, $stateParams, FIREBASE_CONFIG, ratingConfig) {
 	]
 
 	professoresCtrl.atualizaListaProfessores = function(){
-		console.log("adsaad");
+		showLoading();
 		database.ref('/professores/').once('value').then(function(snapshot){
 			console.log("ProfessoresCtrl| consegui um snapshot");
 			snapshot.val().forEach(function(professor){
 				console.log(professor);
 				if(professor) professoresCtrl.professores.push(professor);
 			})
+			hideLoading();
+
+			if(professoresCtrl.professores.length == 0) 
+				professoresCtrl.errorMessage = 'Desculpe não consegui encontrar nenhum professor'
+			else 
+				professoresCtrl.errorMessage = '';
+
 		}, function(error){
 			professoresCtrl.errorMessage = error;
 			console.log(error);
+			hideLoading();
 		});	
-
-		if(professoresCtrl.professores.length == 0) 
-			professoresCtrl.errorMessage = 'Desculpe não consegui encontrar nenhum professor'
 	}
 
 	professoresCtrl.atualizaListaProfessores();
+
 	console.log("ProfessoresCtrl | peguei os professores");
 	console.log(professoresCtrl.professores);
 
